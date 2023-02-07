d07b89edcaf007787a5d817207cf73618344d67d
Kene <kayyungx@gmail.com>
2023-02-07 15:12:08 +0100

    Add food controller actions

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,11 @@
+class FoodsController < ApplicationController
+
+  def index
+    user_id = params[:user_id]
+    @foods = Food.includes(:user_id).where(user_id:)
+  end
+
+  def new
+    @food = Food.new
+  end
+end
