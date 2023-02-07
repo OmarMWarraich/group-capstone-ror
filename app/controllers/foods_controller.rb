a00c437a9f97e9e040c62c5f192e98f43769e41c
Kene <kayyungx@gmail.com>
2023-02-07 22:14:58 +0100

    Add delete button and functionality

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,12 +1,22 @@
 class FoodsController < ApplicationController
 
   def index
-    user_id = params[:user_id]
-  
-    @foods = Food.includes(:user_id).where(user_id:)
+    user_id = created_by
+    @user = User.includes(:foods).find_by(id: user_id)
+    @foods = Food.all
   end
 
   def new
     @food = Food.new
+  end
+
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+    redirect_to foods_index_path
+  end
+
+  def created_by
+    params[:user_id]
   end
 end
