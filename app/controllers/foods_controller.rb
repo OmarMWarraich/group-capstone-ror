0f5cf1bac44415256a67172bd7a4631edd9b94fd
OmarMWarraich <earthling986@gmail.com>
2023-02-08 13:45:15 +0500

    feat: Form AddFood

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -9,13 +9,33 @@ class FoodsController < ApplicationController
     @food = Food.new
   end
 
+  def create
+    @food = Food.new(food_params)
+
+    respond_to do |format|
+      if @food.save
+        format.html { redirect_to foods_index_path, notice: 'Food was successfully created.' }
+        format.json { render :show, status: :created, location: @food }
+      else
+        format.html { render :new }
+        format.json { render json: @food.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def destroy
     @food = Food.find(params[:id])
     @food.destroy
     redirect_to foods_index_path
   end
 
+  private
+
   def created_by
     params[:user_id]
+  end
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price)
   end
 end
