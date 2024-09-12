d1b7314c7b60414f373e8de8594abc3a11b1a5f2
OmarMWarraich <earthling986@gmail.com>
2024-09-13 00:28:12 +0500

    feat: Broadcass

--- app/models/food.rb
+++ app/models/food.rb
@@ -5,5 +5,10 @@ class Food < ApplicationRecord
   has_many :recipes, through: :recipe_foods
   has_many :inventory_foods
   has_many :inventories, through: :inventory_foods
-  
+
+  # broadcasts
+  after_create_commit { broadcast_prepend_to 'foods' }
+  after_update_commit { broadcast_replace_to 'foods' }
+  after_destroy_commit { broadcast_remove_to 'foods' }
+
 end
